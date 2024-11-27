7e2d2839aa766e0e9ebdbd6caf05af1062fa17eb
KrishnaKMA <krishnamallick46@hotmail.com>
2024-11-27 17:09:05 -0500

    Update ExtracurricularActivities.tsx

--- components/ExtracurricularActivities.tsx
+++ components/ExtracurricularActivities.tsx
@@ -4,6 +4,13 @@ import { Activity } from 'lucide-react'
 import { useState } from 'react'
 
 import Image from 'next/image' // Ensure this import is present
+import KarateImage from '../public/images/KaratePhoto.png' // Importing the Karate image
+import BoxingImage from '../public/images/GGG.webp' // Importing the Boxing image
+import GymImage from '../public/images/GymPhoto.jpeg' // Importing the Gym image
+import GamingImage from '../public/images/gaming.jpg' // Importing the Gaming image
+import MusicImage from '../public/images/music.jpg' // Importing the Music image
+import AstronomyImage from '../public/images/Astronomy.jpg' // Importing the Astronomy image
+import GamingVideo from '../public/videos/GamingClip.mp4' // Importing the Gaming video
 
 interface ExtracurricularActivity {
   name: string
@@ -21,42 +28,42 @@ export default function ExtracurricularActivities() {
       name: "Karate",
       description: "I have been practicing karate since gr5, Coach, athlete, strong believer in the Kaizen philosophy",
       icon: <Activity className="w-6 h-6" />,
-      image: "/Images/KaratePhoto.png?height=600&width=800",
+      image: KarateImage,
       additionalInfo: "I am a coach at my Dojo at Kaizen martial arts where I spearhead the training of the competition team alongside my coach Rob Castro. I achieved 1st que brown belt status (One away from black). multiple medals. Represented team Ontario at Nationals.",
     },
     {
       name: "Boxing",
       description: "Training in boxing for 1-2 years, Train regularly and always follow the sport.",
       icon: <Activity className="w-6 h-6" />,
-      image: "/Images/GGG.webp?height=600&width=800",
+      image: BoxingImage,
       additionalInfo: "Boxing is something I have loved for a while, I train at my Dojo Kaizen martial arts and in the picture is my favourite boxer Triple GGG aka Gennady Gennadyevich Golovkin.",
     },
     {
       name: "Gym",
       description: "WE GO JIM!!!",
       icon: <Activity className="w-6 h-6" />,
-      image: "/Images/GymPhoto.jpeg?height=600&width=800",
-      additionalInfo: "I go to the gym often to stay on top of my physical fitness, I mostly train for strength because I think being stronger and lifting heavy is impressive and it also help keep my msucles flexible and loose for sports",
+      image: GymImage,
+      additionalInfo: "I go to the gym often to stay on top of my physical fitness, I mostly train for strength because I think being stronger and lifting heavy is impressive and it also help keep my muscles flexible and loose for sports",
     },
     {
       name: "Gaming",
       description: "Passionate about gaming, exploring new titles, and competing in various online multiplayer games.",
       icon: <Activity className="w-6 h-6" />,
-      image: "/Images/gaming.jpg?height=600&width=800",
+      image: GamingImage,
       additionalInfo: "Gaming is a major hobby for me, and I enjoy playing various genres from strategy to FPS. I also participate in online tournaments and enjoy connecting with the gaming community.",
     },
     {
       name: "Music",
       description: "I play various instruments and have a deep love for both creating and listening to music.",
       icon: <Activity className="w-6 h-6" />,
-      image: "/Images/music.jpg?height=600&width=800",
+      image: MusicImage,
       additionalInfo: "I play guitar and piano, and I also write and produce my own music. I love exploring different genres and experimenting with sounds in my free time, below is one of my favourite acoustic pieces.",
     },
     {
       name: "Astronomy",
       description: "Exploring the universe through stargazing, telescopes, and learning about celestial bodies.",
       icon: <Activity className="w-6 h-6" />,
-      image: "/Images/Astronomy.jpg?height=600&width=800",
+      image: AstronomyImage,
       additionalInfo: "Astronomy has always fascinated me. I enjoy stargazing and learning about the cosmos. I also have a telescope to observe planets, stars, and galaxies.",
     },
   ]
@@ -89,14 +96,14 @@ export default function ExtracurricularActivities() {
               >
                 <p className="text-gray-700">{activity.additionalInfo}</p>
                 {activity.name === "Gaming" ? (
-                  // Update the src here for your local video
+                  // Use the imported Gaming video here
                   <video
                     controls
                     width="100%"
                     height="400"
                     className="rounded-lg"
                   >
-                    <source src="/videos/GamingClip.mp4" type="video/mp4" />
+                    <source src={GamingVideo} type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
                 ) : activity.name === "Music" ? (
