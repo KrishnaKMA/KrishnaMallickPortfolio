aa0e423ee1fec3c7f19965eb1d84e7b93381bb4c
KrishnaKMA <krishnamallick46@hotmail.com>
2025-03-04 02:26:06 -0500

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -75,7 +75,7 @@ export default function Experience() {
   playsInline
   className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
 >
-  <source src="/videos/Experiencebg.mp4" type="video/mp4" />
+  <source src="/videos/ExperienceBg.mp4" type="video/mp4" />
 </video>
 
 
