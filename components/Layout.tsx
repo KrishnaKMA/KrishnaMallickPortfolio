92041707f74fceae2e83ee716436521be7da2543
KrishnaKMA <krishnamallick46@hotmail.com>
2025-03-10 00:53:21 -0400

    errors fixed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,10 @@
 import Header from './Header';
 import Footer from './Footer';
-import ParticlesBackground from '../components/ui/ParticlesBackground'; // Import the ParticlesBackground component
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col min-h-screen">
-            {/* Particle Background */}
-            <ParticlesBackground />
+
 
             {/* Header */}
             <Header />
