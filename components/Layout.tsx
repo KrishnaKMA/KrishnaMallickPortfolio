2241839ce96aa5de4210a74b8adf27849ff8c470
KrishnaKMA <krishnamallick46@hotmail.com>
2025-03-10 00:27:06 -0400

    sunnt undates

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,21 @@
-import Header from './Header'
-import Footer from './Footer'
+import Header from './Header';
+import Footer from './Footer';
+import ParticlesBackground from '../components/ui/ParticlesBackground'; // Import the ParticlesBackground component
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
-  )
-}
+    return (
+        <div className="flex flex-col min-h-screen">
+            {/* Particle Background */}
+            <ParticlesBackground />
 
+            {/* Header */}
+            <Header />
+
+            {/* Main Content */}
+            <main className="flex-grow relative z-10">{children}</main>
+
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
+}
